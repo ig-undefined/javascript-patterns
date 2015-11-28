a474dd1350965f4e0a781e5fdd676d78e33934aa
Igor Cheliadinski <ig.undefined@gmail.com>
2015-11-28 19:01:38 +0300

    Added facade pattern

---
+++ design-patterns/facade/facade.js
@@ -0,0 +1,23 @@
+/**
+ * Created by undefined on 28.11.15.
+ */
+
+var myevent = {
+    stop: function (e) {
+        // other browsers
+        if (typeof e.preventDefault === "function") {
+            e.preventDefault();
+        }
+        if (typeof e.stopPropagation === "function") {
+            e.stopPropagation();
+        }
+
+        // IE
+        if (typeof e.returnValue === "boolean") {
+            e.returnValue = false;
+        }
+        if (typeof e.cancelBubble === "boolean") {
+            e.cancelBubble = true;
+        }
+    }
+};
